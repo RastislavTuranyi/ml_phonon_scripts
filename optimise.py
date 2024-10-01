361fcae054eea15ea335299255b1a1841ff3825e
Turanyi <rastislav.turanyi@stfc.ac.uk>
2024-10-01 15:24:28 +0100

    Switch optimise.py to janus Python API and add constraints

--- optimise.py
+++ optimise.py
@@ -4,6 +4,11 @@ import os
 from shutil import copyfile, rmtree
 import subprocess
 
+from ase.constraints import FixSymmetry
+from ase.io import read, write
+from janus_core.calculations.geom_opt import GeomOpt
+import numpy as np
+
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 SOURCE_DIR = os.path.join(DATA_DIR, 'primitive')
@@ -57,18 +62,59 @@ if __name__ == '__main__':
                 continue
 
         os.makedirs(out_dir)
-        copyfile(file, os.path.join(out_dir, 'POSCAR'))
+        copyfile(file, out_dir)
         os.chdir(out_dir)
 
-        subprocess.run(['janus', 'geomopt',
-                        '--struct', './POSCAR',
-                        '--arch', args.arch,
-                        '--device', 'cuda',
-                        '--out', out_path,
-                        '--calc-kwargs', '{"dispersion": True}',
-                        '--model-path', args.model_path,
-                        cell,
-                        '--fmax', str(FMAX)])
+        filter_kwargs = {'hydrostatic_strain': args.cell,
+                         'scalar_pressure': 0.}
+
+        atoms = read(file, format='vasp')
+        optimiser = GeomOpt(struct=atoms,
+                            arch=args.arch,
+                            device='cuda',
+                            model_path=args.model_path,
+                            calc_kwargs={'dispersion': True},
+                            attach_logger=True,
+                            fmax=FMAX,
+                            write_results=True,
+                            filter_kwargs=filter_kwargs)
+        result = optimiser.run()
+        energy = optimiser.struct.get_potential_energy()
+
+        if optimiser.struct.info['initial_spacegroup'] != optimiser.struct.info['final_spacegroup']:
+            print('Space group changed during optimisation -> retrying with fixed symmetry')
+
+            atoms = read(file, format='vasp')
+            atoms.set_constraint(FixSymmetry(atoms=atoms, adjust_positions=True, adjust_cell=args.cell))
+            optimiser = GeomOpt(struct=atoms,
+                                arch=args.arch,
+                                device='cuda',
+                                model_path=args.model_path,
+                                calc_kwargs={'dispersion': True},
+                                attach_logger=True,
+                                fmax=FMAX,
+                                write_results=True,
+                                filter_kwargs=filter_kwargs)
+            result = optimiser.run()
+
+            energy2 = optimiser.struct.get_potential_energy()
+            print(f'Original energy: {energy}; new energy: {energy2}')
+            energy = energy2
+
+            if optimiser.struct.info['initial_spacegroup'] == optimiser.struct.info['final_spacegroup']:
+                title = 'spacegroup_conserved'
+            else:
+                print('Space group changed despite ASE constraint')
+                title = 'spacegroup_changed'
+        else:
+            title = 'spacegroup_conserved'
+
+        write(out_path, optimiser.struct, format='vasp')
+
+        with open(os.path.join(os.path.dirname(file), title), 'w') as f:
+            f.write(optimiser.struct.info['initial_spacegroup'] + '   ' + optimiser.struct.info['final_spacegroup'])
+
+        np.save(os.path.join(out_path, 'final.npy'), np.array([energy, optimiser.dyn.fmax]))
 
         os.chdir(DATA_DIR)
 
