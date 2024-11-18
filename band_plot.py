c5c359d2768b5efe50dd17cda862b3b5dfec89e5
RastislavTuranyi <rastislav.turanyi@stfc.ac.uk>
2024-11-18 08:20:18 +0000

    Finish band plot

--- band_plot.py
+++ band_plot.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+import numpy as np
+
 import euphonic
 from euphonic.plot import plot_1d
 
@@ -18,24 +20,30 @@ if __name__ == '__main__':
     phonons = euphonic.QpointPhononModes.from_phonopy(path, name)
     phonons.reorder_frequencies()
 
-    bands = phonons.get_dispersion()
-    for band in bands:
-        band.y_data = band.y_data.to('reciprocal_centimeter')
+    bands = phonons.get_dispersion().split(btol=5.)
+    y_max = 0
 
-    bands = bands.split(btol=5.)
+    for band in bands:
+        band._y_data = band.y_data.to('reciprocal_centimeter').magnitude
+        m = np.max(band._y_data)
+        if m > y_max:
+            y_max = m
+
+    if y_max > 3000:
+        y_max = np.ceil(y_max / 500) * 500
+    else:
+        y_max = np.ceil(y_max / 200) * 200
+    #bands = bands.split(btol=5.)
 
     fig = plot_1d(bands, ylabel='Energy (meV)', color='#E94D36', alpha=0.8)
 
     for ax in fig.axes:
-        for line in ax.get_lines():
-            line.set_color('#E94D36')
+        ax.plot(ax.get_xlim(), [0, 0], alpha=0.5, c='#1E5DF8', linestyle=':', linewidth=1)
+        ax.tick_params(labelsize=13)
+        ax.set_ylim(top=y_max)
 
-    fig.axes[-1].set_ylabel('Energy ($cm^{-1}$)', fontsize=20)
+    fig.axes[-1].set_ylabel('Frequency ($cm^{-1}$)', fontsize=20, labelpad=16)
+    
+    fig.tight_layout()
 
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-
-    if os.path.exists(args.output):
-        os.remove(args.output)
-
-    fig.savefig(args.output)
+    fig.savefig(args.output, dpi=2000)
